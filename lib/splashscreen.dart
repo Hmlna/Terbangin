1ae7daf4771d716e197c79d425b4faecd45d7f33
Hmlna <myhanif04@gmail.com>
2025-04-21 09:51:55 +0700

    modify main, login, and add splash screen

---
+++ lib/splashscreen.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:terbangin/home.dart';
+import 'package:terbangin/login.dart';
+import 'package:terbangin/login2.dart';
+
+class Splashscreen extends StatefulWidget {
+  const Splashscreen({super.key});
+
+  @override
+  State<Splashscreen> createState() => _SplashscreenState();
+}
+
+class _SplashscreenState extends State<Splashscreen> {
+  double _opacity = 0.0;
+
+  @override
+  void initState() {
+    super.initState();
+
+    // Mulai fade-in
+    Future.delayed(Duration(milliseconds: 100), () {
+      setState(() {
+        _opacity = 1.0;
+      });
+    });
+
+    // Navigasi ke Home setelah 2.5 detik
+    Future.delayed(Duration(milliseconds: 2500), () {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => const Login2()),
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        color: const Color(0xFF006BFF),
+        child: Center(
+          child: TweenAnimationBuilder<double>(
+            tween: Tween(begin: 0.5, end: 1.0),
+            duration: const Duration(milliseconds: 1500),
+            curve: Curves.easeOut,
+            builder: (context, scale, child) {
+              return AnimatedOpacity(
+                duration: const Duration(milliseconds: 1000),
+                opacity: _opacity,
+                child: Transform.scale(
+                  scale: scale,
+                  child: SvgPicture.asset("assets/plane-icon.svg"),
+                ),
+              );
+            },
+          ),
+        ),
+      ),
+    );
+  }
+}
